b5c3e79008a54c7a3bbef286bcbd5f8e3a3598e6
SIIT_Tae <6522780400@g.siit.tu.ac.th>
2024-11-03 15:30:37 +0700

    fix: some route and db

---
+++ min/src/app/api/students/[studentID]/route.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from "next/server";
+import { createConnection } from "../../../lib/db.js";
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { studentID: string } }
+) {
+  try {
+    console.log("Params:", params);
+    const db = await createConnection();
+    const sql = `SELECT * FROM student WHERE student_ID = ?`;
+    const [student] = await db.query(sql, [params.studentID]);
+
+    if (student.length === 0) {
+      return NextResponse.json(
+        { message: "Student not found" },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(student);
+  } catch (error) {
+    console.error("Database connection failed:", error);
+    return NextResponse.json({ message: error }, { status: 500 });
+  }
+}
